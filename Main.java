bcd77249411c9b63f81a5bfaa26bde156a007b25
Sævar Axel Bjarnason <saevar@Svars-MacBook-Air.local>
2025-08-23 15:41:57 +0000

    Bætti við for loop

--- Main.java
+++ Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Halló heimur");
         System.out.println("Ég heiti Sævar");
+
+        int n = 1;
+
+        for(int i = 0; i<10; i++){
+            n += n;
+            System.out.println(n);
+        }
+        
+
     }
 }
